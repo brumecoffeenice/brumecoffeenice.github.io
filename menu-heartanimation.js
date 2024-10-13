a10ce89f287eb3eff9a788dbf016d26385593607
simon-ameye <ameye.simon@gmail.com>
2024-10-13 18:29:28 +0200

    heart

--- menu-heartanimation.js
+++ menu-heartanimation.js
@@ -6,7 +6,7 @@ const elementPercentInViewport = function (el) {
 	return (
 		Math.min(
 			Math.max(Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100)), 0),
-			Math.max(Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100))
+			Math.max(Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100), 0)
 		)
 	)
 };
@@ -15,10 +15,11 @@ function heartAnimation() {
 	var reviewBoxContainer = document.getElementById("reviewBoxContainer");
 	var heart = document.getElementById("heart");
 
-	if (elementPercentInViewport(reviewBoxContainer) > 50) {
+	var visibleRatio = elementPercentInViewport(reviewBoxContainer);
+	if (visibleRatio > 0) {
 		heart.classList.add('animate');
 	}
-	if (elementPercentInViewport(reviewBoxContainer) < 10) {
+	if (visibleRatio == 0) {
 		heart.classList.remove('animate');
 	}
 }
